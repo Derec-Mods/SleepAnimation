ecf48dc56cd955247fc809cec76f57c50c6716ba
Derek <derex1987@gmail.com>
2026-01-07 14:18:39 -0600

    feat: initial

---
+++ src/main/java/io/github/derec4/sleepAnimation/SleepAnimation.java
@@ -0,0 +1,17 @@
+package io.github.derec4.sleepAnimation;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SleepAnimation extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
